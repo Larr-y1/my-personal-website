d130ad98ee490b6ab45b83d22eca795aae510748
Larry Otieno <larryotieno51@gmail.com>
2025-03-03 07:45:33 +0300

    Adds contact section,Javascript functions and fixing html syntax

--- src/script.js
+++ src/script.js
@@ -7,27 +7,65 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 
+
 /*============ show menu ==============*/
-const navMenu = document.getElementById('nav_menu');
-navToggle = document.getElementById('nav_toggle');
-navClose = document.getElementById('nav_close');
+const navMenu = document.getElementById("nav_menu");
+navToggle = document.getElementById("nav_toggle");
+navClose = document.getElementById("nav_close");
 
 //menu show
 //validates if constant exists
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show_menu');
-    })
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show_menu");
+  });
 }
 
 //Hide show
 //validates if constant exists
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show_menu');
-    })
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show_menu");
+  });
+}
+/*============ Removemenu ==============*/
+const navLink = document.querySelectorAll('.nav_link');
+
+function linkAction() {
+    const navMenu = document.getElementById("nav_menu");
+    //when i click on each link,it will remove the show menu class
+    navMenu.classList.remove("show_menu");
 }
 
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
+
+
+/*============ Style switcher ==============*/
+const styleSwitcherToggle = document.querySelector('.style_switcher_toggler'),
+ styleSwitcher = document.querySelector('.style_switcher');
+
+ styleSwitcherToggle.addEventListener('click', () => {
+    styleSwitcher.classList.toggle('open');
+ });
+
+ //hide switcher on scroll
+ window.addEventListener('scroll', () => {
+    if (styleSwitcher.classList.contains('open')) {
+        styleSwitcher.classList.remove('open');
+    }
+ });
+
+ const alternateStyles = document.querySelectorAll('link.alternate_styles');
+
+ function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute('title')) {
+            style.removeAttribute('disabled');
+        } else {
+            style.setAttribute('disabled', true);
+        }
+    });
+ }
 
 
